3c6e6dbf0feb7eecb85f458f7daf428a8169ac1c
mahmoud-el-tohamy <mahmoud.eltohamyy@gmail.com>
2025-04-16 23:22:29 +0200

    Add GitHub Pages deployment configuration

--- chefio/next.config.ts
+++ chefio/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import next from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/chefio',
+  assetPrefix: '/chefio/'
 };
 
 export default nextConfig;
