da998c4b19e4875b46fee87acbc1dae6effc5817
mahmoud-el-tohamy <mahmoud.eltohamyy@gmail.com>
2025-06-11 02:22:27 +0300

    fix: uncomment and structure currentUser export

--- app/constants/currentUser.ts
+++ app/constants/currentUser.ts
@@ -1,3 +1,9 @@
-// export const currentUser = {
-//   username: 'mahmoud_eltohamy',
-// };
+export const currentUser = {
+  username: 'mahmoud_eltohamy',
+  name: 'Mahmoud El-Tohamy',
+  avatar: '/images/avatars/mahmoud.jpg',
+  email: 'mahmoud@example.com',
+  recipes: 0,
+  following: 0,
+  followers: 0
+};
